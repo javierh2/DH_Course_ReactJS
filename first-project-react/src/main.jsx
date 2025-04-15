4258e5f009d9d192a1a991a48006e7e75feca5de
javier <javier.morabaiz@gmail.com>
2025-04-15 15:50:05 -0300

    haciendo uso de un componente pricipal y usando dentro de la estructura principal de react

--- first-project-react/src/main.jsx
+++ first-project-react/src/main.jsx
@@ -1,10 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { PorfolioApp } from './PorfolioApp'
 // import './index.css'
 // import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <PorfolioApp/>
   </StrictMode>,
 )
