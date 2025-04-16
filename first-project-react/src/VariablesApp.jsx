81e1c7b04bd41e99252623ca9a9e65e5eb7e439f
javier <javier.morabaiz@gmail.com>
2025-04-16 17:04:11 -0300

    fragmentos y estilos en CSS

--- first-project-react/src/VariablesApp.jsx
+++ first-project-react/src/VariablesApp.jsx
@@ -1,7 +1,17 @@
 import React from 'react'
+import './VariableApp.css'
 
 export const VariablesApp = () => {
+  const active = true
+  const style ={
+    color : active ? 'red' : 'blue',
+    fontSize : '20px'
+  }
   return (
-    <div>VariablesApp</div>
+    <>
+      {/* <h1 className={active ? 'active' : 'inactive'}>Clase de Estilos</h1> */}
+
+      <h1 style={style}> OTRA FORMA</h1>
+    </>
   )
 }
