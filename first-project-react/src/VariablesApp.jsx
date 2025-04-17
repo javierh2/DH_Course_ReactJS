3d366a3e4c0bf6be1c148d9d8b24ce7230c158a8
javier <javier.morabaiz@gmail.com>
2025-04-17 15:47:10 -0300

    componentes dentro de otro componente

--- first-project-react/src/VariablesApp.jsx
+++ first-project-react/src/VariablesApp.jsx
@@ -1,25 +1,17 @@
 import React from 'react'
 import './VariableApp.css'
-import PropTypes from 'prop.types'  // se importa el modulo de que tipo de propiedad será
 
-export const VariablesApp = ({titulo,subtitulo,num}) => {  // desde el main
+// uso de un funcional component dentro de otro
+// aunque se encapsula dentro del export para ser enviado
+// se puede consumir solo desde adentro
+const Button = ({title}) =>{
+  return <button>{title}</button>
+}
 
+export const VariablesApp = () => {
   return (
     <>
-      <h1>{titulo}</h1>
-      <h2>{subtitulo}</h2>
-      <h3>{num}</h3>
+      <Button title='Soy un functionarrow button'/>
     </>
   )
-}
-
-// obligatorio y un tipo de dato especifico Y REQUERIDO
-VariablesApp.propTypes={
-    titulo: PropTypes.string.isRequired,
-    num: PropTypes.number.isRequired
-}
-
-VariablesApp.defaultProps={
-    titulo: 'curso react',
-    subtitulo : 'seccion de propiedades'
 }
--- first-project-react/src/main.jsx
+++ first-project-react/src/main.jsx
@@ -4,8 +4,8 @@ import { VariablesApp } from './VariablesApp'
 import './style.css'
 
 
-createRoot(document.getElementById('root')).render( // se conecta con la app y el componente  si no está la aclaración se usarán las DEFAULT
+createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <VariablesApp titulo='este es un titulo' subtitulo='este es un subtitulo' num={999}/>
+    <VariablesApp />
   </StrictMode>,
 )
