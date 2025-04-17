0a136d7153f0c09df103675db39a7ce86f586eba
javier <javier.morabaiz@gmail.com>
2025-04-17 13:30:36 -0300

    props

--- first-project-react/src/VariablesApp.jsx
+++ first-project-react/src/VariablesApp.jsx
@@ -1,15 +1,25 @@
 import React from 'react'
 import './VariableApp.css'
+import PropTypes from 'prop.types'  // se importa el modulo de que tipo de propiedad será
 
-export const VariablesApp = ({objeto} ) => {
-  console.log(objeto)
+export const VariablesApp = ({titulo,subtitulo,num}) => {  // desde el main
 
   return (
     <>
-      <h1>PROPS:</h1>
-      <h2>{JSON.stringify(objeto.nombre)}</h2>
-      <h2>{JSON.stringify(objeto.apellido)}</h2>
-      <p>{objeto.nombre}</p>
+      <h1>{titulo}</h1>
+      <h2>{subtitulo}</h2>
+      <h3>{num}</h3>
     </>
   )
+}
+
+// obligatorio y un tipo de dato especifico Y REQUERIDO
+VariablesApp.propTypes={
+    titulo: PropTypes.string.isRequired,
+    num: PropTypes.number.isRequired
+}
+
+VariablesApp.defaultProps={
+    titulo: 'curso react',
+    subtitulo : 'seccion de propiedades'
 }
