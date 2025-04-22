af646579690ccb6b62b974aaeb965c965943669a
javier <javier.morabaiz@gmail.com>
2025-04-22 19:07:33 -0300

    test de fetch y uso del hook useEffect

--- first-project-react/src/pages/users/UserApp.jsx
+++ first-project-react/src/pages/users/UserApp.jsx
@@ -1,7 +1,34 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 export const UserApp = () => {
-  return (
-    <div>UserApp</div>
-  )
+
+    const urlBase = 'https://jsonplaceholder.typicode.com/users'
+
+    const [users, setUsers] = useState([])
+
+    const fetchUsers = async () => {
+        try {
+            const res = await fetch(urlBase)
+            const data = await res.json()
+            console.log(data)
+            setUsers(data)
+        } catch (error) {
+            console.error(error)
+        }
+    }
+
+    useEffect(() => {
+        fetchUsers()
+    }, [])
+
+
+    // fetch(urlBase)
+    //     .then(response => response.json())
+    //     .then(json => console.log(json))
+
+    return (
+        <>
+        {users.map(user => <p key={user.id}>{user.name}</p>)}
+        </>
+    )
 }
