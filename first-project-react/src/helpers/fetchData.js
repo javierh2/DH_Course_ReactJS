4c961021a2eb0d3619cca7c43e12e735eef86f41
javier <javier.morabaiz@gmail.com>
2025-04-24 16:56:51 -0300

    CUSTOMHOOK Y HELPERS -  minimo codigo posible en cada archivo

---
+++ first-project-react/src/helpers/fetchData.js
@@ -0,0 +1,20 @@
+export const fetchData = async ({endpoint}) => {
+
+    const urlBase = `https://jsonplaceholder.typicode.com/${endpoint}`
+
+    try {
+        const res = await fetch(urlBase)
+        const data = await res.json()
+        console.log(data)
+        return{
+            data,
+            isLoading: false
+        }
+    } catch (error) {
+        console.error(error)
+        return {
+            isLoading: false
+        }
+
+    }
+}
--- first-project-react/src/pages/users/components/FetchList.jsx
+++ first-project-react/src/pages/users/components/FetchList.jsx
@@ -1,29 +1,19 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
+import { useFetchData } from '../../../hooks/useFetchData'
 
 export const FetchList = ({endpoint}) => {
 
-    const urlBase = `https://jsonplaceholder.typicode.com/${endpoint}`
+    const {data, isLoading} = useFetchData({endpoint})
 
-    const [items, setItems] = useState([])
-
-    const fetchItems = async () => {
-        try {
-            const res = await fetch(urlBase)
-            const data = await res.json()
-            console.log(data)
-            setItems(data)
-        } catch (error) {
-            console.error(error)
-        }
-    }
-
-    useEffect(() => {
-        fetchItems()
-    }, [endpoint])
 
     return (
         <>
-            {items.map(item => <p key={item.id}>{item.name}</p>)}
+            { isLoading
+            ? <p>Cargando...</p>
+                :endpoint == 'users'
+                    ? data.map(item => <p key={item.id}>{item.name}</p>)
+                    : data.map(item => <p key={item.id}>{item.body}</p>)
+            }
         </>
     )
 }
