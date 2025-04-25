929ddf123721b04b36b39234b2a983c8eb6600d8
javier <javier.morabaiz@gmail.com>
2025-04-25 14:43:29 -0300

    proy de practica HOOKS y bootstrap

--- proyecto-hooks-react/src/main.jsx
+++ proyecto-hooks-react/src/main.jsx
@@ -1,10 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import { HookApp } from './pages/HookApp'
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-
+  <HookApp/>
   </StrictMode>,
 )
---
+++ proyecto-hooks-react/src/pages/HookApp.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const HookApp = () => {
+  return (
+    <>
+    <h1>APLICACION DE HOOKS</h1>
+    <button type="button" className="btn btn-success">EXITO</button>
+    <button type="button" className="btn btn-danger">PELIGRO</button>
+    </>
+  )
+}
