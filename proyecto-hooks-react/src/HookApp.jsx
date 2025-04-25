1ed64ede9e10edb26f67155b9da076370f5d9a22
javier <javier.morabaiz@gmail.com>
2025-04-25 19:32:29 -0300

    custom hook

--- proyecto-hooks-react/src/HookApp.jsx
+++ proyecto-hooks-react/src/HookApp.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import { CounterApp } from './components/CounterApp'
 
 export const HookApp = () => {
   return (
     <>
-    <h1>APLICACION DE HOOKS</h1>
-    <button type="button" className="btn btn-success">EXITO</button>
-    <button type="button" className="btn btn-danger">PELIGRO</button>
+      <h1>APLICACION DE HOOKS</h1>
+      <hr />
+      <CounterApp/>
     </>
   )
 }
--- proyecto-hooks-react/src/main.jsx
+++ proyecto-hooks-react/src/main.jsx
@@ -1,7 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import { HookApp } from './pages/HookApp'
+import { HookApp } from './HookApp'
+
 
 
 createRoot(document.getElementById('root')).render(
