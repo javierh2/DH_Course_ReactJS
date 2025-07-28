fda84db06fc8460bd2bfb510dfa949f6964ba28a
javier <javier.morabaiz@gmail.com>
2025-07-28 20:14:12 -0300

    hooks prueba de form y counter

--- proyecto-hooks-react/src/hooks/useForm.js
+++ proyecto-hooks-react/src/hooks/useForm.js
@@ -4,7 +4,6 @@ import { useState } from 'react';
 export const useForm = (initialForm = {}) => {
 
     const [formState, setFormState] = useState(initialForm);
-    const { username, nickname, email, password } = formState;
 
     const onInputChange = ({target}) => {
         const { name, value } = target;
