3e2b0044c60c5a3bc75a05bde97ea9156ec10953
javier <javier.morabaiz@gmail.com>
2025-07-28 19:57:14 -0300

    hooks prueba de form y counter

--- proyecto-hooks-react/src/HookApp.jsx
+++ proyecto-hooks-react/src/HookApp.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { CounterApp } from './components/CounterApp'
+import { FormsApp } from './components/FormsApp'
 
 export const HookApp = () => {
   return (
     <>
       <h1>APLICACION DE HOOKS</h1>
       <hr />
-      <CounterApp/>
+      {/* <CounterApp/> */}
+      <FormsApp/>
     </>
   )
 }
---
+++ proyecto-hooks-react/src/components/FormsApp.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import { useForm } from '../hooks/useForm';
+
+export const FormsApp = () => {
+
+    const initialForm = {
+        username: '',
+        nickname: '',
+        email: '',
+        password: ''
+    };
+
+    const {formState, onInputChange} = useForm(initialForm);
+    const { username, nickname, email, password } = formState;
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log(formState);
+    };
+
+    return (
+        <>
+            <h4>Formulario Test</h4>
+            <br />
+            <form onSubmit={handleSubmit}>
+                <div className="mb-3">
+                    <label htmlFor="username" className="form-label">User name</label>
+                    <input type="text" className="form-control" id="username" name="username" value={username} onChange={onInputChange}/>
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="nickname" className="form-label">Nick name</label>
+                    <input type="text" className="form-control" id="nickname" name="nickname" value={nickname} onChange={onInputChange}/>
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="email" className="form-label">Email</label>
+                    <input type="email" className="form-control" id="email" name="email" value={email} onChange={onInputChange}/>
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="password" className="form-label">Password</label>
+                    <input type="password" className="form-control" id="password" name="password" value={password} onChange={onInputChange}/>
+                </div>
+                <button type="submit" className="btn btn-primary">Submit</button>
+            </form>
+        </>
+    )
+}
