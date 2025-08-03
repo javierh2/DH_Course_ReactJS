3ca366072121b313098c32cffc5ff00fba0bb29f
javier <javier.morabaiz@gmail.com>
2025-08-03 13:13:59 -0300

    rename de los componentes en Ingles

--- proyecto-hooks-react/src/components/TaskComponent.jsx
+++ proyecto-hooks-react/src/components/TaskComponent.jsx
@@ -2,13 +2,67 @@ import React from 'react'
 
 export const TaskComponent = () => {
 
-    const taskReducer
+    const inicialState = [
+        { id: 1, title: 'Explicar reducers', completed: false },
+    ]
 
+    const newTask = { id: 2, title: 'Hacer reducers', completed: false}
 
+    const renameTask = { id: 1, title: 'Explicar y leer reducers', completed: true }
 
+    const createTask = {
+        type: '[Tasks] Agregar tarea',
+        payload: newTask
+    }
+
+    const editTask = {
+        type: '[Tasks] Editar tarea',
+        payload: renameTask
+    }
+
+    const deleteTask = {
+        type: '[Tasks] Eliminar tarea',
+
+    }
+
+    const clearTasks = {
+        type: '[Tasks] Limpiar tareas',
+
+    }
+
+    const taskReducer = (state = inicialState, action = {}) => {
+        switch (action.type) {
+            case '[Tasks] Agregar tarea':
+                return [...state, action.payload]
+
+            case '[Tasks] Editar tarea':
+                return state.map(task => {
+                    if (task.id === action.payload.id) {
+                        return {
+                            ...task,
+                            ...action.payload
+                        }
+                    }
+                    return task
+                })
+
+            case '[Tasks] Eliminar tarea':
+                return state.filter(task => task.id !== action.payload.id)
+
+            case '[Tasks] Limpiar tareas':
+                return []
+
+            default:
+                return state
+        }
+    }
+
+    console.log(taskReducer(inicialState, createTask));
 
 
     return (
-        <div>TareasComponent</div>
+        <>
+            {/* {JSON.stringify(taskReducer(inicialState, createTask))} */}
+        </>
     )
 }
